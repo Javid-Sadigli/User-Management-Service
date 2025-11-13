bf9d1d949bb87097c1b3a18d6ebf16fbbc4c93cc
Javid Sadigli <cavidoffical@gmail.com>
2025-11-13 23:51:37 +0400

    Added user mappings

--- src/main/java/com/looyt/usermanagement/mapper/UserMapper.java
+++ src/main/java/com/looyt/usermanagement/mapper/UserMapper.java
@@ -2,8 +2,13 @@ package com.looyt.usermanagement.mapper;
 
 import org.mapstruct.Mapper;
 
+import com.looyt.usermanagement.model.dto.request.UserRequest;
+import com.looyt.usermanagement.model.dto.response.UserResponse;
+import com.looyt.usermanagement.model.entity.UserEntity;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper 
 {
-    
+    public UserResponse entityToResponse(UserEntity entity); 
+    public UserEntity requestToEntity(UserRequest request); 
 }
