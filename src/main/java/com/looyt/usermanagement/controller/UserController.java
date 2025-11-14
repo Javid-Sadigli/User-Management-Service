860e6eb85c74c4cd3543d3f65c63fe8cf6c48bf4
Javid Sadigli <cavidoffical@gmail.com>
2025-11-14 10:57:35 +0400

    Added get user by id endpoint

--- src/main/java/com/looyt/usermanagement/controller/UserController.java
+++ src/main/java/com/looyt/usermanagement/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.looyt.usermanagement.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.looyt.usermanagement.service.UserService;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import com.looyt.usermanagement.model.dto.response.BaseResponse;
+import com.looyt.usermanagement.model.dto.response.UserResponse;
+import com.looyt.usermanagement.service.UserService;
 
 @Slf4j
 @RestController
@@ -17,4 +21,11 @@ public class UserController
     private static final String LOG_TEMPLATE = "{} request to /user{}";
 
     private final UserService userService; 
+
+    @GetMapping("/{id}")
+    public BaseResponse<UserResponse> getUserById(@PathVariable long id)
+    {
+        log.info(LOG_TEMPLATE, "GET", "/" + id);
+        return userService.getUserById(id); 
+    }
 }
