a94065664b4f2e5ab149e6b4f3c3d070f11fb571
Javid Sadigli <cavidoffical@gmail.com>
2025-11-15 18:53:12 +0400

    Added test server endpoint

---
+++ src/main/java/com/looyt/usermanagement/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.looyt.usermanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController 
+{
+    @GetMapping("/test-server")
+    public String testServer()
+    {
+        return "Server is running.";
+    }        
+}
