d06e85dd0dc1364b27d1d3d25c7ea64d17e4e3f6
Javid Sadigli <cavidoffical@gmail.com>
2025-11-18 17:48:03 +0400

    Created failing tests

--- src/test/java/com/looyt/usermanagement/service/UserServiceTests.java
+++ src/test/java/com/looyt/usermanagement/service/UserServiceTests.java
@@ -258,7 +258,7 @@ public class UserServiceTests
     {
         return UserResponse.builder()
             .id(1L)
-            .email("example@mail.com")
+            .email("example@mail.commmmmmmmmmm") // This one
             .fullName("Test User")
             .userRole(UserRole.EMPLOYEE)
             .build(); 
